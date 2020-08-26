2db5337328709522beea5b099157657a6f833687
anuraj <anurajguha@gmail.com>
2020-08-26 22:10:18 +0530

    add edit option to note card

--- src/components/Dashboard/NotesList/NoteCard/NoteCard.js
+++ src/components/Dashboard/NotesList/NoteCard/NoteCard.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, CardContent, Grid, Typography, CardActions, IconButton, makeStyles } from '@material-ui/core'
 import { Delete as DeleteIcon } from '@material-ui/icons'
 import { deleteNoteAction } from '../../../../store/notes/noteActionCreators'
 import { connect } from 'react-redux'
 import { decrementTotalNotes } from '../../../../store/summary/summaryActionCreators'
 import EditIcon from '@material-ui/icons/Edit'
+import EditModal from '../../../UI/EditModal'
 
 const useStyles = makeStyles({
     card: {
@@ -19,10 +20,17 @@ const useStyles = makeStyles({
 const NoteCard = (props) => {
     const classes = useStyles()
 
+    const [editModaOpen, setEditModalOpen] = useState(false)
+
     const handleDelete = (id) => {
         //add additional logic if needed
         props.deleteNote(id)
         props.updateTotalNotes()
+    }
+
+    const handleEdit = () => {
+        console.log('edit window opened')
+        setEditModalOpen(true)
     }
 
     return (
@@ -38,12 +46,13 @@ const NoteCard = (props) => {
                         <IconButton onClick={() => handleDelete(props.id)}>
                             <DeleteIcon color="secondary"/>
                         </IconButton>
-                        <IconButton onClick={() => console.log('edit icon clicked')}>
+                        <IconButton onClick={handleEdit}>
                             <EditIcon color="secondary"/>
                         </IconButton>
                     </CardActions>
                 </Card>
             </Grid>
+            <EditModal open={editModaOpen} handleClose={() => setEditModalOpen(false)} text={props.text} id={props.id} />
         </>
     )
 }
---
+++ src/components/UI/EditModal.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import { connect } from 'react-redux'
+import { updateNoteAction } from '../../store/notes/noteActionCreators'
+
+const EditModal = (props) => {
+    const [formText, setFormText] = useState('')
+
+    //componentDidMount
+    useEffect(() => {
+        setFormText(props.text)
+    }, [])
+
+    const handleChange = (e) => {
+        setFormText(e.target.value)   
+    }
+    const handleSubmit = () => {
+        console.log('form edited')
+        //update the note in store
+        props.updateNote(props.id, formText)
+        props.handleClose()
+    }
+    const handleCancel = () => {
+        setFormText(props.text)
+        props.handleClose()
+    }
+    return (
+        <div>
+            <Dialog open={props.open} onClose={props.handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title"> Edit </DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        id="name"
+                        color="secondary"
+                        value={formText}
+                        onChange={handleChange}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCancel} color="secondary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleSubmit} color="secondary">
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    )
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updateNote: (id, newText) => dispatch(updateNoteAction(id, newText))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(EditModal)
